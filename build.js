af47da5da0d966a6b7726402d6ab86662d8a8e10
joesinghaus <deceptive.duality@gmail.com>
2018-03-06 20:33:38 +0100

    Make build process smoother
    Will now fail gracefully if babel is not installed. Whitespace handled
    more consistently.

--- build.js
+++ build.js
@@ -10,31 +10,32 @@ const pug = require('pug'),
 	fs = require('fs'),
 	execSync = require('child_process').execSync;
 
-const enableBabel = config.enableBabel,
-	options = {
-		data: JSON.parse(fs.readFileSync('Source/data.json', 'utf8')),
-		pretty: config.pretty,
-		translation: JSON.parse(fs.readFileSync('translation.json', 'utf8'))
-	},
-	workerSrc = `\n"use strict";\n` +
-		`const data=${fs.readFileSync('Source/data.json', 'utf8')};` +
-		fs.readFileSync('Source/sheetworkers.js', 'utf8');
+const options = {
+	data: JSON.parse(fs.readFileSync('Source/data.json', 'utf8')),
+	pretty: config.pretty,
+	translation: JSON.parse(fs.readFileSync('translation.json', 'utf8')),
+	workers: `\n"use strict";\n` +
+		`const data=${fs.readFileSync('Source/data.json', 'utf8').replace(/\n$/, '')};\n` +
+		`${fs.readFileSync('Source/sheetworkers.js', 'utf8').replace(/\n$/, '')}\n`
+};
 
 // Handle presence/absence of babel
-if (enableBabel && !options.pretty) {
-	const babel = require('jstransformer')(require('jstransformer-babel'));
-	options.workers = '\n' + babel.render(workerSrc, {presets: ['minify']}).body + '\n';
-}
-else {
-	options.workers = workerSrc;
+if (config.enableBabel && !config.pretty) {
+	try {
+		const babel = require('jstransformer')(require('jstransformer-babel'));
+		options.workers = `\n${babel.render(options.workers, {presets: ['minify']}).body}\n`;
+	} catch (err) {
+		console.log('jstransformer or jstransformer-babel not found. Proceeding without minifying sheet workers.');
+	}
 }
 
 // Build CSS file
-execSync('scss --default-encoding=UTF-8 --unix-newlines --sourcemap=none --no-cache --style '+ (options.pretty ? 'expanded' : 'compressed') + ' Source/blades.scss blades.css');
+execSync('scss --default-encoding=UTF-8 --unix-newlines --sourcemap=none --no-cache --style '+ (config.pretty ? 'expanded' : 'compressed') + ' Source/blades.scss blades.css');
 // repair bogus @charset directive
-if (options.pretty) fs.writeFileSync('blades.css', fs.readFileSync('blades.css', 'utf8').replace(/@charset "UTF-8";(?:\n|\r\n)?/, ''));
+fs.writeFileSync('blades.css', fs.readFileSync('blades.css', 'utf8').replace(/^@charset "UTF-8";(?:\n|\r\n)?/, ''));
 
 // Build HTML
-fs.writeFileSync('blades.html', pug.renderFile('Source/Blades.pug', options));
+const htmlOutput = pug.renderFile('Source/Blades.pug', options).replace(/\n\n/g, '\n').replace(/^\n/g, '');
+fs.writeFileSync('blades.html', htmlOutput);
 
-console.log('Sheet build completed');
+console.log('Sheet build completed.');
